8b1c9fa11b14b5c9cf883748d01ee5ef2c174c2f
ArilAsghaf <61927378+ArilAsghaf@users.noreply.github.com>
2020-04-11 21:31:30 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Inheritance
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person person = new Person("Iwang", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Arif", 50, "6946", "chemistry");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Annida", 20, "3301", "anidalutfi97@gmail.com");
+            student.GetNameAndAge();
+        }
+    }
+}
